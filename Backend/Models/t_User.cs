7bf8a7307049503ee5a0fe788c8d3e65b2c032ab
intern050 <95472181+SatasiyaShyam@users.noreply.github.com>
2023-03-20 14:38:26 +0530

    hgf

--- Backend/Models/t_User.cs
+++ Backend/Models/t_User.cs
@@ -2,7 +2,15 @@ namespace Backend.Models
 {
     public class t_User
     {
+        public int UserId { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Contact { get; set; }
+        public string Image { get; set; }
+        public bool isTraveler { get; set; }
     }
 }
--- Backend/Models/vm_Login.cs
+++ Backend/Models/vm_Login.cs
@@ -2,15 +2,7 @@ namespace Backend.Models
 {
     public class vm_Login
     {
-        public int UserId { get; set; }
-        public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Gender { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Contact { get; set; }
-        public string Image { get; set; }
-        public bool isTraveler { get; set; }
     }
 }
