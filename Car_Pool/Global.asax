01ad516866db7a5e06fd97c9a412a05a5f14730b
intern050 <95472181+SatasiyaShyam@users.noreply.github.com>
2023-03-20 12:34:45 +0530

    aa

---
+++ Car_Pool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Car_Pool.MvcApplication" Language="C#" %>
---
+++ Car_Pool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Car_Pool.Startup))]
+namespace Car_Pool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
